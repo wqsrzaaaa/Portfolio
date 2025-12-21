0446ff769215dffab6e8d410ea5393760ab3dc0c
Waqas Raza <wqsrza07@gmail.com>
2025-12-22 00:20:58 +1100

    new changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div className = "w-full md:px-10 px-5 h-12 border-t border-zinc-400 flex items-center justify-between">
-      <p>2023</p>
-        <h2 className='text-xl font-bold'>Waqas Raza</h2>
-      <p>Present</p>
-    </div>
-  )
-}
+    <footer className="w-full md:px-10 px-5 h-16 border-t border-zinc-400 flex flex-col md:flex-row items-center justify-between gap-2 md:gap-0">
+      <p className="text-sm text-gray-500">2023</p>
+      <h2 className="text-lg md:text-xl font-bold">Waqas Raza</h2>
+      <p className="text-sm text-gray-500">Present</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
